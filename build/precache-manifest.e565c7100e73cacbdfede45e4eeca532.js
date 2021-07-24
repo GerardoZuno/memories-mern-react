6d4aa40cbe92c8f968da09bf6587698afb5b6e61
GerardoZuno <gerardozunounitec@gmail.com>
2021-07-24 16:08:18 -0500

    Finished Front-End

---
+++ build/precache-manifest.e565c7100e73cacbdfede45e4eeca532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a28d4815da187562865dcdaf8316ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec94675a45dab705ee4c",
+    "url": "/static/css/main.d58b00a3.chunk.css"
+  },
+  {
+    "revision": "7c81b832c4ca5942cdb3",
+    "url": "/static/js/2.c182b085.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.c182b085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec94675a45dab705ee4c",
+    "url": "/static/js/main.e0b2e3d0.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
